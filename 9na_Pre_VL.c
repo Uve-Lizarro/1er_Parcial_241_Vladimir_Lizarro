5e57e1c13b4764714c35b601a12dc1ec9efae77b
Uve-Lizarro <168239171+Uve-Lizarro@users.noreply.github.com>
2024-05-11 22:12:13 -0400

    Update 9na_Pre_VL.c

--- 9na_Pre_VL.c
+++ 9na_Pre_VL.c
@@ -10,12 +10,12 @@ void generaSerie(int i, int n, int *v) {
 void main(int argc, char** argv) { 
     int m;
     int n;
-    int ragno;
+    int rango;
     int nroPro;
     MPI_Init(&argc, &argv); 
-    MPI_Comm_rank(MPI_COMM_WORLD, &ragno); 
+    MPI_Comm_rank(MPI_COMM_WORLD, &rango); 
     MPI_Comm_size(MPI_COMM_WORLD, &nroPro); 
-    if (ragno==0) { 
+    if (rango==0) { 
         printf("Numero de vectores: "); 
         fflush(stdout); 
         scanf("%d", &m); 
@@ -27,7 +27,7 @@ void main(int argc, char** argv) {
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD); 
     int vR[n]; 
     MPI_Barrier(MPI_COMM_WORLD);
-    for (int i = ragno; i < m; i += nroPro) { 
+    for (int i = rango; i < m; i += nroPro) { 
         generaSerie(i,n,vR); 
         printf("%d: ", i); 
         for (int j = 0; j < n; j++) { 
