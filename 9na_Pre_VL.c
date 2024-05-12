7666e0cc303887cc8fa447e5cca20b63381b6287
Uve-Lizarro <168239171+Uve-Lizarro@users.noreply.github.com>
2024-05-11 22:01:33 -0400

    Add files via upload

---
+++ 9na_Pre_VL.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <mpi.h>
+//Vladimir Ariel Lizarro Velasquez
+void generaSerie(int i, int n, int *v) { 
+    for (int j = 0; j < n; j++) { 
+        int x=(i*n+j+1)*2; 
+        v[j]=x; 
+    } 
+} 
+void main(int argc, char** argv) { 
+    int m;
+    int n;
+    int ragno;
+    int nroPro;
+    MPI_Init(&argc, &argv); 
+    MPI_Comm_rank(MPI_COMM_WORLD, &ragno); 
+    MPI_Comm_size(MPI_COMM_WORLD, &nroPro); 
+    if (ragno==0) { 
+        printf("Numero de vectores: "); 
+        fflush(stdout); 
+        scanf("%d", &m); 
+        printf("Numero de terminos: "); 
+        fflush(stdout); 
+        scanf("%d", &n); 
+    } 
+    MPI_Bcast(&m, 1, MPI_INT, 0, MPI_COMM_WORLD); 
+    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD); 
+    int vR[n]; 
+    MPI_Barrier(MPI_COMM_WORLD);
+    for (int i = ragno; i < m; i += nroPro) { 
+        generaSerie(i,n,vR); 
+        printf("%d: ", i); 
+        for (int j = 0; j < n; j++) { 
+            printf("%d ", vR[j]); 
+        } 
+        printf("\n"); 
+    } 
+    MPI_Finalize();
+}
